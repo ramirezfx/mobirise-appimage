fb00416683de3d99bc3c40169f92b95ae21d5938
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2021-09-07 09:40:39 +0200

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+WORKDIR=mobirise.AppDir
+rm -Rf $WORKDIR
+rm mobirise5beta-setup.deb
+rm appimagetool-x86_64.AppImage
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+wget https://download.mobirise.com/beta/mobirise5beta-setup.deb
+chmod 700 appimagetool-x86_64.AppImage
+mkdir $WORKDIR
+dpkg -x mobirise5beta-setup.deb $WORKDIR
+cp AppRun $WORKDIR
+cp mobirise.desktop $WORKDIR
+cp mobirise.png $WORKDIR
+mkdir $WORKDIR/usr/bin
+cd $WORKDIR/usr/bin && ln -s ../../opt/Mobirise/mobirise mobirise
+cd ../../../
+./appimagetool-x86_64.AppImage $WORKDIR
