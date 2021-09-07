c3a13630332cc3315ee35bfd3b8c766e0b0c544d
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2021-09-07 09:44:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ rm appimagetool-x86_64.AppImage
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
 wget https://download.mobirise.com/beta/mobirise5beta-setup.deb
 chmod 700 appimagetool-x86_64.AppImage
+chmod 700 AppRun
+chmod 700 mobirise.desktop
 mkdir $WORKDIR
 dpkg -x mobirise5beta-setup.deb $WORKDIR
 cp AppRun $WORKDIR
